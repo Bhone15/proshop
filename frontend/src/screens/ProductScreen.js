12b6e5542f8cf55d6f3684f298ffe082f5a7fe08
Bhone Pyae Aung <bhonepyaeaung15@gmail.com>
2023-03-03 11:45:40 +0630

    done add/remove cart function

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -16,7 +16,7 @@ import Loader from "../components/Loader";
 import { productDetails } from "../actions/productActions";
 
 const ProductScreen = () => {
-  const [qty, setQty] = useState(0);
+  const [qty, setQty] = useState(1);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { id } = useParams();
