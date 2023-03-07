58ce781f1376c0dcd6c09f50c579566bf605d7b0
Bhone15 <bhonepyaeaung15@gmail.com>
2023-03-07 09:58:50 +0630

    get all user route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -96,3 +96,11 @@ export const updateUserProfile = asyncHandler(async (req, res) => {
     throw new Error('User not found');
   }
 });
+
+// @desc    GET all user
+// @route   GET /api/users
+// @access  Private/Admin
+export const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+});
